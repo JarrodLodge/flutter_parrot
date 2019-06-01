4e89fd8ac7362b584456b24d6ea010726d6f4930
Jarrod <jarrodlodge@gmail.com>
2019-06-01 16:21:51 +1200

    Added Main screen
    Main screen design

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Parrot',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF49b046),
+        accentColor: Color(0xFFfcee05),
+        // scaffoldBackgroundColor: Color(0xff7de375)
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -28,64 +31,138 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildHero() {
+    return Stack(
+      alignment: Alignment.center,
+      overflow: Overflow.visible,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                  width: 1.5,
+                  style: BorderStyle.solid),
+              borderRadius: BorderRadius.circular(15)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 130),
+                Text(
+                  'Welcome to Flutter Parrot, the voice to widget search tool',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: -100,
+          right: 120,
+          child: Image.asset('assets/logo.png'),
+        ),
+        Positioned(
+          top: -40,
+          right: 20,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 30,
+                child: Icon(
+                  Icons.arrow_left,
+                  size: 55,
+                ),
+              ),
+              Container(
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                          color: Colors.black,
+                          width: 1.5,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.circular(35)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text('Try saying "Show me a Row Widget"'),
+                  ))
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Parrot'),
+    return Stack(children: <Widget>[
+      Image.asset(
+        "assets/bg.png",
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
       ),
-      body: Center(
-        child: Column(
+      Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Parrot'),
+        ),
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                  color: Colors.grey[200],
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Welcome to Flutter Parrot, the voice to widget search tool',
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  )),
-            )
+            Center(
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0), child: _buildHero())),
           ],
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
               ),
-            ),
-            ...widgetInfoList.map((w) {
-              return ListTile(
-              title: Text(w.title),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ResultsPage(w.id)),
+              ...widgetInfoList.map((w) {
+                return ListTile(
+                  title: Text(w.title),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ResultsPage(w.id)),
+                    );
+                  },
                 );
-              },
-            );
-            }).toList()
-          ],
+              }).toList()
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          tooltip: 'Increment',
+          child: Icon(Icons.mic),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.mic),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ]);
   }
+}
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(size.width, 0.0);
+    path.lineTo(size.width / 2, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(TriangleClipper oldClipper) => false;
 }
