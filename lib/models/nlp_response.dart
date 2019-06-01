b0df71f3380ceec59297ee9730d46e59e39e07ae
Julius Spencer <jspencer@juliusspencer.co.nz>
2019-06-01 14:40:17 +1200

    Small hack to parse json

--- lib/models/nlp_response.dart
+++ lib/models/nlp_response.dart
@@ -2,7 +2,17 @@ class NlpResponse {
   final List<NlpResponseItem> response;
 
   NlpResponse(this.response);
-  NlpResponse.fromJson(Map<String, dynamic> json) : response = json['answers'];
+
+  static NlpResponse fromJson(Map<String, dynamic> json) {
+    List<NlpResponseItem> items = List<NlpResponseItem>();
+
+    List<dynamic> decoded = json['answers'];
+    for (var i = 0; i < decoded.length; i++) {
+      var item = NlpResponseItem.fromJson(decoded[i]);
+      items.add(item);
+    }
+    return NlpResponse(items);
+  }
 }
 
 class NlpResponseItem {
